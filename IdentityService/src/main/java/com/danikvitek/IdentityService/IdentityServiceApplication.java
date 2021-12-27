742c2ba8f280c280927c2fadd32b8f024e02f718
Danik Vitek <x3665107@gmail.com>
2021-12-28 00:27:19 +0200

    Ready for committing to archistrator

--- IdentityService/src/main/java/com/danikvitek/IdentityService/IdentityServiceApplication.java
+++ IdentityService/src/main/java/com/danikvitek/IdentityService/IdentityServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IdentitySercidceApplication {
+public class IdentityServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IdentitySercidceApplication.class, args);
+        SpringApplication.run(IdentityServiceApplication.class, args);
     }
 
 }
